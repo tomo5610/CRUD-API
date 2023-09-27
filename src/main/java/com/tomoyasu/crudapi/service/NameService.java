f38e659392787ec07445eb09939555858bbdf609
tomoyasu <tomoyasu0504.9115@gmail.com>
2023-09-27 21:18:38 +0900

    YearMonthを実装

--- src/main/java/com/tomoyasu/crudapi/service/NameService.java
+++ src/main/java/com/tomoyasu/crudapi/service/NameService.java
@@ -2,6 +2,7 @@ package com.tomoyasu.crudapi.service;
 
 import com.tomoyasu.crudapi.entity.Name;
 
+import java.time.YearMonth;
 import java.util.List;
 
 public interface NameService {
@@ -9,9 +10,9 @@ public interface NameService {
 
     Name findById(int id);
 
-    Name createName(String name, String birth);
+    Name createName(String name, YearMonth birth);
 
-    void updateName(int id, String name, String birth) throws Exception;
+    void updateName(int id, String name, YearMonth birth) throws Exception;
 
     void deleteById(int id);
 }
